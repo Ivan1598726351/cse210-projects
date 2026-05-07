5aea4fffdeea0fe83bbe1adb1578da3732a29ef9
Ivan1598726351 <iversic7@gmail.com>
2026-05-07 11:15:53 +0200

    I completed the exercise and compared it to solution

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        //Console.Write("Pick the magic number? ");
+        //string magic_number = Console.ReadLine();
+        //int number = int.Parse(magic_number);
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        int guess = -1;
+        while (guess != number)
+        {    
+            Console.Write("What is your guess? ");
+            string guess_number = Console.ReadLine();
+            guess = int.Parse(guess_number);
+            if (guess < number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed the number");
+            }
+        }
     }
 }
