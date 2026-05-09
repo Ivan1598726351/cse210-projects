e2490197fb23ec501e5bd796582a64e32add2336
Ivan1598726351 <iversic7@gmail.com>
2026-05-09 21:11:00 +0200

    Fixed the code regarding the main function

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -5,46 +5,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Main();
-        static void Main()
-        {
-            DisplayMessage();
-            string user_name=PropmptUserName();
-            int favorite_number=PropmptUserNumber();
-            int squared = SquareNumber(favorite_number);
-            DisplayResult(squared,user_name);
-
-            static void DisplayMessage()
-            {
-                Console.WriteLine("Welcome to the program!");
-            }
-
-            static string PropmptUserName()
-            {
-                Console.Write("Enter your name ");
-                string user_name = Console.ReadLine();
-                return user_name;
-            }
-            static int PropmptUserNumber()
-            {
-                Console.Write("Enter your favorite number ");
-                string fav_number= Console.ReadLine();
-                int favorite_number=int.Parse(fav_number);
-                return favorite_number;
-            }        
-
-            static int SquareNumber(int number)
-            {
-                int squared = number*number;
-                return squared;
-            }
         
-            static void DisplayResult(int number, string name)
-            {
-                Console.WriteLine($"Hello {name}, the square of your favorite number is {number}");
+        
+        
+        DisplayMessage();
+        string user_name=PropmptUserName();
+        int favorite_number=PropmptUserNumber();
+        int squared = SquareNumber(favorite_number);
+        DisplayResult(squared,user_name);
 
-            }
-                    
+        static void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to the program!");
         }
+
+        static string PropmptUserName()
+        {
+            Console.Write("Enter your name ");
+            string user_name = Console.ReadLine();
+            return user_name;
+        }
+        static int PropmptUserNumber()
+        {
+            Console.Write("Enter your favorite number ");
+            string fav_number= Console.ReadLine();
+            int favorite_number=int.Parse(fav_number);
+            return favorite_number;
+        }        
+
+        static int SquareNumber(int number)
+        {
+            int squared = number*number;
+            return squared;
+        }
+        
+        static void DisplayResult(int number, string name)
+        {
+            Console.WriteLine($"Hello {name}, the square of your favorite number is {number}");
+
+        }
+                    
+        
     }
 }
