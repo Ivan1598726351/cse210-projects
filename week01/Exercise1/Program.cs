eb8a7f41976e0789f00154054e4170a23e31f731
Ivan1598726351 <iversic7@gmail.com>
2026-05-09 21:05:14 +0200

    Used the different names for variables, that "fit better"

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,9 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("What is your name? ");
-        string name = Console.ReadLine();
+        string first = Console.ReadLine();
         Console.Write("What is your last name? ");
-        string last_name = Console.ReadLine();
-        Console.WriteLine($"Your name is {last_name}, {name} {last_name}.");
+        string last = Console.ReadLine();
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
