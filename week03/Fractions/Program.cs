e627eece65e25508de4b23bca3a67b04fdb475f7
Ivan1598726351 <iversic7@gmail.com>
2026-05-16 10:26:37 +0200

    DId the exercise

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFractionString(f1.GetTop(),f1.GetBottom()));
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetFractionString(f2.GetTop(),f2.GetBottom()));
+        Fraction f3 = new Fraction(5,71);
+        Console.WriteLine(f3.GetFractionString(f3.GetTop(),f3.GetBottom()));
+        Fraction f4 = new Fraction();
+        f4.setTop(4);
+        f4.setBottom(5);
+        Console.WriteLine(f4.GetFractionString(f4.GetTop(),f4.GetBottom()));
+        Console.WriteLine(f4.GetDecimalNumber(f4.GetTop(),f4.GetBottom()));
     }
 }
